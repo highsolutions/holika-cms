a277043e2931328f8b09cd80f2af45221c3eb15e
Hubert Sitarski <hubertsitarski@gmail.com>
2021-05-12 13:54:13 +0200

    [feature] Contact Form

--- api/contact-form/controllers/contact-form.js
+++ api/contact-form/controllers/contact-form.js
@@ -7,19 +7,31 @@
 
 module.exports = {
   async send(ctx) {
-    await strapi.plugins['email'].services.email.send({
-      to: process.env.EMAIL_ADDRESS_FROM,
-      from: ctx.request.body.email,
-      replyTo: ctx.request.body.email,
-      subject: 'Wiadomość z formularza kontaktowego',
-      html: "<p>" + ctx.request.body.message + "</p><br>" +
-        "<p><b>Imię i nazwisko:</b> " + ctx.request.body.fullName + "</p>" +
-        "<p><b>Telefon:</b> " + ctx.request.body.tel + "</p>" +
-        "<p><b>Nazwa firmy:</b> " + ctx.request.body.companyName + "</p>",
-    });
+    let error = false;
 
-    return {
-      "status": "ok"
-    };
+    if (!ctx.request.body.email || !ctx.request.body.message) {
+      error = true;
+    }
+
+    if (error == false) {
+      await strapi.plugins['email'].services.email.send({
+        to: process.env.EMAIL_ADDRESS_FROM,
+        from: ctx.request.body.email,
+        replyTo: ctx.request.body.email,
+        subject: 'Wiadomość z formularza kontaktowego',
+        html: "<p>" + ctx.request.body.message + "</p><br>" +
+          "<p><b>Imię i nazwisko:</b> " + ctx.request.body.fullName + "</p>" +
+          "<p><b>Telefon:</b> " + ctx.request.body.tel + "</p>" +
+          "<p><b>Nazwa firmy:</b> " + ctx.request.body.companyName + "</p>",
+      });
+
+      return {
+        statusCode: 200,
+        message: 'ok'
+      };
+    }
+     else {
+      return ctx.response.badRequest('Missing required fields');
+    }
   }
 };
